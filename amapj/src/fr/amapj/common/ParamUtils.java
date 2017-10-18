799449f7fb0946049340a32dfe0916cb1fa85fd2
Antoine Laborderie <alaborderie@lekooa.com>
2017-10-18 16:53:23 +0200

    [WiP] Parsing JSON

--- amapj/src/fr/amapj/common/ParamUtils.java
+++ amapj/src/fr/amapj/common/ParamUtils.java
@@ -1,5 +1,31 @@
-package fr.amapj.common;
+// package fr.amapj.common;
+
+import com.google.gson.Gson;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ParamUtils {
-    public static Boolean canConsumerChangePaymentValues = false;
+
+    public static boolean canConsumerChangePaymentValues;
+
+    public static void main (String [] args) {
+        ParamUtils.init("/Users/antoine/IUT/workspace/amapj-1/amapj/src/config.json");
+    }
+
+    public static void init(String path)
+    {
+        Gson gson = new Gson();
+        try {
+            System.out.println(canConsumerChangePaymentValues);            
+            canConsumerChangePaymentValues = gson.fromJson(new FileReader(path), ParamUtils.class).canConsumerChangePaymentValues;
+            System.out.println(canConsumerChangePaymentValues);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+        }
+    }
+
+    
 }
